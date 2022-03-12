cde97e03c542e10174606923901be3dea6a80e2e
Bety Martinez <trism460@gmail.com>
2022-03-12 12:31:14 -0600

    Delete contacts in tab3

--- contactsApplication/src/app/tab3/tab3.page.ts
+++ contactsApplication/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactInfoService } from '../services/contact-info.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contacts: any[];
 
-  constructor() {}
+  constructor(public alert: AlertController, public contactInfo: ContactInfoService) {
+    this.contacts = contactInfo.contacts;
+  }
 
+  async showAlert(headerA, subheaderA, messageA, button) {
+    const addAlert = await this.alert.create({
+      header: headerA,
+      subHeader: subheaderA,
+      message: messageA,
+      buttons: button
+    });
+    await addAlert.present();
+  }
+  async deleteContact(i) {
+    await this.showAlert
+    ('Eliminar contacto',
+    '¿Está seguro de eliminar el contacto?',
+    'Esta acción no se puede deshacer',
+    [
+      {
+        text: 'OK',
+        handler: () => {
+          this.contacts.splice(i, 1);
+        }
+      },
+      {
+        text: 'Cancel',
+      }
+    ]);
+  }
 }
